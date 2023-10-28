b138d5086855f0dda9165db1d3e15c3c2fbbfb07
Shimon Moyal <shimon.m@minute.ly>
2023-10-28 20:31:29 +0300

    generating documentation

---
+++ lesson-03/getDurationImproved.js
@@ -0,0 +1,36 @@
+/**
+ * Converts an ISO 8601 duration format string into its total seconds representation.
+ * 
+ * @function
+ * @name getDuration
+ * @param {string} duration - An ISO 8601 duration format string (e.g., "1D2H3M4S").
+ * @returns {number} Total duration in seconds.
+ * @throws {Error} Throws an error if the provided duration is not in the valid ISO 8601 format.
+ * 
+ * @example
+ * const durationInSeconds = getDuration("1D2H3M4S");
+ * console.log(durationInSeconds);  // Outputs: 93784
+ */
+function getDuration(duration) {
+    if (typeof duration !== 'string' || !/^(\d+D)?(\d+H)?(\d+M)?(\d+S)?$/.test(duration)) {
+        throw new Error('Invalid ISO 8601 duration format.');
+    }
+
+    const SECONDS_IN_DAY = 86400;
+    const SECONDS_IN_HOUR = 3600;
+    const SECONDS_IN_MINUTE = 60;
+
+    const matches = duration.match(/(\d+D)?(\d+H)?(\d+M)?(\d+S)?/);
+    const days = parseInt(matches[1] || 0, 10);
+    const hours = parseInt(matches[2] || 0, 10);
+    const minutes = parseInt(matches[3] || 0, 10);
+    const seconds = parseInt(matches[4] || 0, 10);
+
+    return days * SECONDS_IN_DAY + hours * SECONDS_IN_HOUR + minutes * SECONDS_IN_MINUTE + seconds;
+}
+
+// Test the function
+const testDuration = "1H"; // ISO 8601 duration format
+
+module.exports = getDuration;
+
---
+++ lesson-03/readme.md
@@ -0,0 +1,40 @@
+# `getDuration` Function Documentation
+
+The `getDuration` function is designed to convert an ISO 8601 duration format string into its total seconds representation. This documentation covers the function's purpose, usage, testing, and design choices.
+
+## Table of Contents
+
+- [Function Purpose](#function-purpose)
+- [Usage](#usage)
+- [Testing](#testing)
+- [Design Choices](#design-choices)
+- [Future Improvements](#future-improvements)
+
+## Function Purpose
+
+The main purpose of the `getDuration` function is to provide a standardized way to convert ISO 8601 duration strings (e.g., "1D2H3M4S") into their equivalent total duration in seconds.
+
+## Usage
+
+To use the `getDuration` function:
+
+```javascript
+const durationInSeconds = getDuration("1D2H3M4S");
+console.log(durationInSeconds);  // Outputs: 93784
+
+## Parameters
+- **duration (String)**: An ISO 8601 duration format string.
+
+## Returns
+- **Integer**: Total duration in seconds.
+
+## Testing
+We use Jest as our testing framework. The tests ensure:
+- Correct conversion of valid ISO 8601 duration strings.
+- Proper error handling for invalid duration formats.
+- Handling of non-string inputs.
+
+To run the tests:
+```bash
+npm test
+
